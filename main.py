91c1c7f2393baae5bf80c821ec6357061bfe5f0a
JuanJm <devjuanjm@gmail.com>
2022-06-24 14:28:24 -0500

    Creacion de funcion printedNumbers

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 def Sum(x, y):
     return x + y
 
+def printedNumbers():
+    for i in range(0;1000):
+        if i % 2 == 0:
+            print(i)
+
 a = 3
 b = 2
 z = Sum(a,b)
 print("El resultado es {}".format(z))
+printedNumbers()
+
+
 
